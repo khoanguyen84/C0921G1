8b408bd4a542ab13b26bf371bb46965c1d0360a3
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2021-10-15 13:48:28 +0700

    loop statement

---
+++ Loop/loop.js
@@ -0,0 +1,78 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// let i = 1;
+// for(; i <= 10; i++){
+//     console.log(i);
+// }
+// console.log(`i = ${i}`);
+
+// let i = 1;
+// for(; i <= 10;){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 1;
+// for(; ;){
+//     console.log(i);
+//     if(i >=10){
+//         break;
+//     }
+//     i++;
+// }
+
+// for(let i = 1; i <= 10; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i=2; i<=10; i+=2){
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 10; i++){
+//     if(i%2 != 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+// let total = 0;
+// for(let i=1; i<=10; i++){
+//     total +=i;
+// }
+// console.log(total);
+
+// for(var i=1, total = 0; i<=10; i++){
+//     total +=i;
+// }
+// console.log(total);
+
+// for(var i=1, total = 0; i<=10; i++){
+//     total +=i;
+//     if(total > 5){
+//         break;
+//     }
+// }
+// console.log(total);
+
+// for(var i=1, total = 0; i<=10 && total <= 5; i++){
+//     total +=i;
+// }
+// console.log(total);
+
+// let i=11;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// let i= 11;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<=10);
+
