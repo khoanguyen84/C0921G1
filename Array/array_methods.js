8fbf29a8f82a8d946d38172344c8559081ffe654
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2021-10-19 09:52:54 +0700

    array methods

---
+++ Array/array_methods.js
@@ -0,0 +1,61 @@
+let array = [2, 30, 4, 5, 3, 4, 5, 6];
+// array.forEach(function(value, index){
+//     console.log(index);
+// })
+
+array.forEach(each);
+function each(value, index, arr){
+    console.log(value);
+}
+// array.sort(function(number1,number2){
+//     return number2 - number1;
+// });
+// array.sort(compare);
+// function compare (number1,number2){
+//     return number2 - number1;
+// }
+// console.log(array.toString());
+// console.log(`Max : ${array[0]}`);
+// console.log(`Min : ${array[array.length-1]}`);
+// let chars = ["Banana", "Orange", "Apple", "Mango","apple"];
+// console.log(chars.sort());
+// console.log(chars.reverse());
+// console.log(array.sort());
+// let a = array.slice (1, 5 );
+// console.log(a);
+// let arr = [...array];
+// array.pop();
+// console.log(array.toString());
+// console.log(arr.toString());
+// let number = [10,11,12];
+// let tempo = array.concat (number);
+// let tempo2 = number.concat (array);
+// console.log(tempo.toString());
+// console.log(tempo2.toString());
+// array.splice(array.length ,0,7);
+// // array.splice(2,3,7,7,7);
+
+// // // array.splice (3,0, 9 ,9);
+
+// console.log(array.toString());
+// // // array.splice(3,2);
+// console.log(array.toString());
+// delete array[3];
+// console.log(array[3]);
+// console.log(array.toString());
+// console.log(array.toString());
+
+// console.log(array.join());
+// console.log(array.join('+'));
+
+// console.log(array.pop());
+
+// array.push(7);
+
+// console.log(array.push(8,9,10));
+// array.push([1,1,1]);
+//  console.log(array.toString())
+
+// console.log(array.shift());
+
+// console.log(array.unshift(9,9))
